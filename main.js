98b6478c81099d5fbfb8cd1c55bf3f648da27282
Marcelo Aparecido Freitas Vieira <marcelo.freitas@santamarcelina.edu.br>
2026-03-25 11:19:09 -0300

    Adicionando novos livros: Felipe Augusto e O Ritmo de Ludo e Siso

--- main.js
+++ main.js
@@ -22,7 +22,9 @@ const books = [
     { id: 21, title: 'Davi, Vivi e os Sprunkis das Boas Escolhas no Santa Marcelina', discipline: 'Convívio Social', category: 'Comportamento', year: 'Convívio Social', url: 'https://gemini.google.com/share/e7ceaa786fef', views: 120, icon: 'ph-users' },
     { id: 22, title: 'A Sala que Virou Mundo: As Aventuras Geopolíticas de Bernardo', discipline: 'Geografia', category: 'Geografia', year: '8º Ano', url: 'https://gemini.google.com/share/c59ddd90202d', views: 100, icon: 'ph-globe' },
     { id: 23, title: 'Dignidade em Campo: A Jornada do Gabriel', discipline: 'Ensino Religioso', category: 'Ensino Religioso', year: '8º Ano', url: 'https://gemini.google.com/share/a946ef337cfb', views: 130, icon: 'ph-hands-praying' },
-    { id: 24, title: 'O Pequeno Grande Cidadão: Davi e o Ritmo da Responsabilidade', discipline: 'Ensino Religioso', category: 'Ensino Religioso', year: '7º Ano', url: 'https://gemini.google.com/share/86c3ffddfa1a', views: 140, icon: 'ph-hands-praying' }
+    { id: 24, title: 'O Pequeno Grande Cidadão: Davi e o Ritmo da Responsabilidade', discipline: 'Ensino Religioso', category: 'Ensino Religioso', year: '7º Ano', url: 'https://gemini.google.com/share/86c3ffddfa1a', views: 140, icon: 'ph-hands-praying' },
+    { id: 25, title: 'Felipe Augusto e o Expresso dos Números Mágicos', discipline: 'Matemática', category: 'Matemática', year: '3º Ano', url: 'https://gemini.google.com/share/3684f98e8bcb', views: 90, icon: 'ph-math-operations' },
+    { id: 26, title: 'O Ritmo de Ludo e Siso', discipline: 'Convívio Social', category: 'Comportamento', year: 'Convívio Social', url: 'https://gemini.google.com/share/f7ded8e40951?hl=pt', views: 110, icon: 'ph-users' }
 ];
 
 const disciplines = [...new Set(books.map(b => b.discipline))];
